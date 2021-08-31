883fd08a4855ff61f424c968cd192b3ae2c62bac
Erdeni <batorov-1995@mail.ru>
2021-08-31 21:17:00 +0300

    Отправка ответ на сервер через query params

--- src/components/Quest.vue
+++ src/components/Quest.vue
@@ -1,30 +1,30 @@
 <template>
   <b-card
-    no-body
-    class="card"
-    style="max-width: 100%;"
-    v-if="!isFinish"
+      no-body
+      class="card"
+      style="max-width: 100%;"
+      v-if="count !== lastPage"
   >
     <b-row no-gutters class="bg-color row">
       <b-col md :class="bgName" class="global-bg">
         <b-img-lazy
-          class="mirror-img"
-          v-if="result === null"
-          :src="require('@/assets/image/mirror-normal.png')"
+            class="mirror-img"
+            v-if="result === null"
+            :src="require('@/assets/image/mirror-normal.png')"
         />
         <b-img-lazy
-          class="mirror-img"
-          v-else-if="isRight"
-          :src="require('@/assets/image/mirror-nice.png')"
+            class="mirror-img"
+            v-else-if="isRight"
+            :src="require('@/assets/image/mirror-nice.png')"
         />
         <b-img-lazy
-          class="mirror-img"
-          v-else
-          :src="require('@/assets/image/mirror-bad.png')"
+            class="mirror-img"
+            v-else
+            :src="require('@/assets/image/mirror-bad.png')"
         />
         <b-img-lazy
-          :src="require('@/assets/image/helm.png')"
-          class="helm-img"
+            :src="require('@/assets/image/helm.png')"
+            class="helm-img"
         />
       </b-col>
 
@@ -38,10 +38,10 @@
                 Где эта Дорога?
               </b-card-title>
               <button
-                class="quest-btn"
-                v-for="quest in item.quests"
-                :key="quest.id"
-                @click="pageResult(quest.outcome, item.rightAnswer)"
+                  class="quest-btn"
+                  v-for="quest in item.quests"
+                  :key="quest.id"
+                  @click="pageResult(quest.outcome, item.rightAnswer)"
               >
                 {{ quest.version }}
               </button>
@@ -51,9 +51,9 @@
               {{ result }}
               <div class="mt-4" v-html="item.promo"></div>
               <button
-                class="main-btn mb-5 mt-5 w-25"
-                type="button"
-                @click="nextPage"
+                  class="main-btn mb-5 mt-5 w-25"
+                  type="button"
+                  @click="nextPage"
               >
                 Далее
               </button>
@@ -69,7 +69,7 @@
       <span>© 2020</span>
     </div>
   </b-card>
-  <finish v-else :rightAnswers="rightAnswers" @clearDB="clearDB" @restart="restart"></finish>
+  <finish v-else :answer="answer" @restart="restart"></finish>
 </template>
 
 <script>
@@ -80,8 +80,6 @@ export default {
     return {
       records: [],
       answer: [],
-      rightAnswers: [],
-      isFinish: false,
       isRight: false,
       result: null,
       count: 1,
@@ -97,49 +95,22 @@ export default {
       alert(error)
     }
   },
-  watch: {
-     count: function (val){
-       if(val === this.lastPage) {
-        this.finishResult()
-       }
-     }
-  },
   methods: {
     restart() {
       this.count = 1
-      this.isFinish = false
+      this.answer = []
     },
-    clearDB() {
-      this.answer.forEach(async (item) => {
-         await fetch(`http://localhost:3000/answers/${item.id}`, {
-          method: 'DELETE',
-        })
-      })
-
-    },
-   async finishResult() {
-      const res = await fetch(`http://localhost:3000/answers`)
-        this.answer = await res.json()
-     this.rightAnswers = this.answer.filter(item => {
-       return this.records.find(record => item.outcome ===  record.rightAnswer )
-     })
-     this.isFinish = true
-    },
-   async pageResult(outcome, right) {
-      const res = await fetch(`http://localhost:3000/answers`, {
-          method: 'POST',
-          body: JSON.stringify({
-              id: Date.now(),
-              outcome
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-      })
-     const json = await res.json();
-      this.result = json.outcome
-      if (this.result === right) {
-        this.isRight = true
+    async pageResult(outcome) {
+      try {
+        const res = await fetch(`http://localhost:3000/question?rightAnswer=${outcome}`)
+        const data = await res.json();
+        if (data.length > 0) {
+          this.answer.push(data)
+          this.isRight = true
+        }
+        this.result = outcome
+      } catch (e) {
+        alert(e.message)
       }
     },
     nextPage() {
@@ -148,15 +119,17 @@ export default {
       this.isRight = false
     }
   },
-
   computed: {
     pageCount() {
       return this.records.filter(el => el.id === this.count)
     },
+
     bgName() {
       return "bg-" + this.count
     }
   },
-  components: {Finish}
+  components: {
+    Finish
+  }
 }
 </script>
